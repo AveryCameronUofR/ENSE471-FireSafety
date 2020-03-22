68520cd9a3c69f1de8ed3dbb8ae12ff4524fdb10
Avery <averycameron35@gmail.com>
2020-03-22 17:29:58 -0600

    Fixed Script styling to fill screen

--- Screens/ScriptScreen.tsx
+++ Screens/ScriptScreen.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const ScriptScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Script</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 export default ScriptScreen;
