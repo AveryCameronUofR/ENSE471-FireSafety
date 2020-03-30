f0fec025a845aff9c6f9c9580c4cf6938e7514fc
Avery <averycameron35@gmail.com>
2020-03-29 22:38:52 -0600

    Upated audio to load sounds once and replayAsync to avoid errors
    By reloading and playing the sounds each button press, after 15 presses the audio stopped playing. By declaring the sound objects outside of the function and using replayAsync instead of playAsync errors with sound and resources are resolved.

--- Deliverable 4 - MVP/helpers/audio.tsx
+++ Deliverable 4 - MVP/helpers/audio.tsx
@@ -1,69 +1,89 @@
 import { Audio } from "expo-av";
+import Sound from "react-native-sound";
+let soundObject1 = new Audio.Sound();
+soundObject1.loadAsync(require("../assets/audio/keypress1.mp3"));
+let soundObject2 = new Audio.Sound();
+soundObject2.loadAsync(require("../assets/audio/keypress2.mp3"));
+let soundObject3 = new Audio.Sound();
+soundObject3.loadAsync(require("../assets/audio/keypress3.mp3"));
+let soundObject4 = new Audio.Sound();
+soundObject4.loadAsync(require("../assets/audio/keypress4.mp3"));
+let soundObject5 = new Audio.Sound();
+soundObject5.loadAsync(require("../assets/audio/keypress5.mp3"));
+let soundObject6 = new Audio.Sound();
+soundObject6.loadAsync(require("../assets/audio/keypress6.mp3"));
+let soundObject7 = new Audio.Sound();
+soundObject7.loadAsync(require("../assets/audio/keypress7.mp3"));
+let soundObject8 = new Audio.Sound();
+soundObject8.loadAsync(require("../assets/audio/keypress8.mp3"));
+let soundObject9 = new Audio.Sound();
+soundObject9.loadAsync(require("../assets/audio/keypress9.mp3"));
+let soundObjectStar = new Audio.Sound();
+soundObjectStar.loadAsync(require("../assets/audio/keypressStar.mp3"));
+let soundObject0 = new Audio.Sound();
+soundObject0.loadAsync(require("../assets/audio/keypress0.mp3"));
+let soundObjectPound = new Audio.Sound();
+soundObjectPound.loadAsync(require("../assets/audio/keypressPound.mp3"));
 
 export const playButtonSound = async (num: string) => {
-  if (Audio.getPermissionsAsync()) {
-    const soundObject = new Audio.Sound();
-    try {
-      //create the sound object
-      //require requires a static file location, values are hardcoded for that reason
-      switch (num) {
-        case "1": {
-          await soundObject.loadAsync(require("../assets/audio/keypress1.mp3"));
-          break;
-        }
-        case "2": {
-          await soundObject.loadAsync(require("../assets/audio/keypress2.mp3"));
-          break;
-        }
-        case "3": {
-          await soundObject.loadAsync(require("../assets/audio/keypress3.mp3"));
-          break;
-        }
-        case "4": {
-          await soundObject.loadAsync(require("../assets/audio/keypress4.mp3"));
-          break;
-        }
-        case "5": {
-          await soundObject.loadAsync(require("../assets/audio/keypress5.mp3"));
-          break;
-        }
-        case "6": {
-          await soundObject.loadAsync(require("../assets/audio/keypress6.mp3"));
-          break;
-        }
-        case "7": {
-          await soundObject.loadAsync(require("../assets/audio/keypress7.mp3"));
-          break;
-        }
-        case "8": {
-          await soundObject.loadAsync(require("../assets/audio/keypress8.mp3"));
-          break;
-        }
-        case "9": {
-          await soundObject.loadAsync(require("../assets/audio/keypress9.mp3"));
-          break;
-        }
-        case "*": {
-          await soundObject.loadAsync(
-            require("../assets/audio/keypressStar.mp3")
-          );
-          break;
-        }
-        case "0": {
-          await soundObject.loadAsync(require("../assets/audio/keypress0.mp3"));
-          break;
-        }
-        case "#": {
-          await soundObject.loadAsync(require("../assets/audio/keypressPound.mp3"));
-          break;
-        }
-        default: {
-          break;
-        }
+  const soundObject = new Audio.Sound();
+  try {
+    //create the sound object
+    //require requires a static file location, values are hardcoded for that reason
+    switch (num) {
+      case "1": {
+        soundObject1.replayAsync();
+        break;
       }
-      await soundObject.playAsync();
-    } catch (error) {
-      // An error occurred!
+      case "2": {
+        soundObject2.replayAsync();
+        break;
+      }
+      case "3": {
+        soundObject3.replayAsync();
+        break;
+      }
+      case "4": {
+        soundObject4.replayAsync();
+        break;
+      }
+      case "5": {
+        soundObject5.replayAsync();
+        break;
+      }
+      case "6": {
+        soundObject6.replayAsync();
+        break;
+      }
+      case "7": {
+        soundObject7.replayAsync();
+        break;
+      }
+      case "8": {
+        soundObject8.replayAsync();
+        break;
+      }
+      case "9": {
+        soundObject9.replayAsync();
+        break;
+      }
+      case "*": {
+        soundObjectStar.replayAsync();
+        break;
+      }
+      case "0": {
+        soundObject0.replayAsync();
+        break;
+      }
+      case "#": {
+        soundObjectPound.replayAsync();
+        break;
+      }
+      default: {
+        break;
+      }
     }
+  } catch (error) {
+    // An error occurred!
   }
 };
