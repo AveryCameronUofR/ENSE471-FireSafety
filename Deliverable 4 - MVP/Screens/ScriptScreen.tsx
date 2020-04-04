d8e831707664fe7f4f543d3ba70d3dbc0ca8d66c
Avery <averycameron35@gmail.com>
2020-04-03 18:16:25 -0600

    Added Bullet Points to ScriptScreen

--- Deliverable 4 - MVP/Screens/ScriptScreen.tsx
+++ Deliverable 4 - MVP/Screens/ScriptScreen.tsx
@@ -8,92 +8,89 @@ const ScriptScreen = () => {
         911 Call Example:
       </Text>
       <Text style={styles.infoText}>
-        {`
-          911: What is your emergency?
-          Caller: There is a Fire.
-          911: Where is the emergency?
-          Caller: [Address, Description]
-          911: What is your name?
-          Caller: [Name]
-          911: What is your telephone number?
-          Caller: [Telephone number]
-          911: Do you need police or an ambulance?
-          Caller:
-        `}
+{`911: What is your emergency?
+Caller: There is a Fire.
+911: Where is the emergency?
+Caller: [Address, Description]
+911: What is your name?
+Caller: [Name]
+911: What is your telephone number?
+Caller: [Telephone number]
+911: Do you need police or an ambulance?
+Caller:
+`}
       </Text>
-      <Text style={styles.headerText}>
-        Additional Fire Questions:
+      <Text style={styles.subheaderText}>
+        Additional Fire Questions:🔥
       </Text>
+      <Text style={styles.emojis}></Text>
       <Text style={styles.infoText}>
-        {`
-          Are you in danger?
-          Is anyone trapped or injured?
-          Are smoke and flames visible?
-          What type of fire are you reporting?
-        `}
+{`\u2022 Are you in danger?
+\u2022 Is anyone trapped or injured?
+\u2022 Are smoke and flames visible?
+\u2022 What type of fire are you reporting?
+`}
       </Text>
 
       <Text style={styles.headerText}>
         911 Safety Tips
       </Text>
       <Text style={styles.subheaderText}>
-        Police emergencies
+        Police emergencies🚓
       </Text>
       <Text style={styles.infoText}>
-        {`
-          Are you in danger, or is someone's life in danger?
-          If a crime was committed, can you provide a description of the offender?
-          Are there any weapons involved?
-        `}
+{`\u2022 Are you in danger, or is someone's life in danger?
+\u2022 If a crime was committed, can you provide a description of the offender?
+\u2022 Are there any weapons involved?
+`}
       </Text>
       <Text style={styles.subheaderText}>
-        Medical emergencies
+        Medical emergencies🏥
       </Text>
       <Text style={styles.infoText}>
-        {`
-          Is the person conscious? Breathing? What else can you tell about the person's condition?
-          Is the person able to tell you anything about the problem he/she is experiencing?
-          Are you able to provide emergency first aid? Is it safe to do so?
-        `}
+{`\u2022 Is the person conscious? Breathing? What else can you tell about the person's condition?
+\u2022 Is the person able to tell you anything about the problem he/she is experiencing?
+\u2022 Are you able to provide emergency first aid? Is it safe to do so?
+`}
       </Text>
       <Text style={styles.subheaderText}>
-        Rescue situations
+        Rescue situations⛑
       </Text>
       <Text style={styles.infoText}>
-        {`
-          How many people are trapped or injured or in need of rescue?
-          If the situation involves vehicles, what types of vehicles are involved and how many?
-          Are there any other hazards present?
-          Is there a risk of fire?
-        `}
+{`\u2022 How many people are trapped or injured or in need of rescue?
+\u2022 If the situation involves vehicles, what types of vehicles are involved and how many?
+\u2022 Are there any other hazards present?
+\u2022 Is there a risk of fire?
+`}
       </Text>
       <Text style={styles.subheaderText}>
-        Dangerous goods situations
+        Dangerous goods situations☢
       </Text>
       <Text style={styles.infoText}>
-        {`
-          Is fuel or other dangerous cargo leaking?
-          Is there a sign with an identification number or symbol on the side, front or back of the vehicle or vehicles? 
-          If so, can you identify what the symbol is?
-        `}
+{`\u2022 Is fuel or other dangerous cargo leaking?
+\u2022 Is there a sign with an identification number or symbol on the side, front or back of the vehicle or vehicles? 
+\u2022 If so, can you identify what the symbol is?
+`}
         </Text>
       <Text style={styles.subheaderText}>
         Prank calls to 911
       </Text>
       <Text style={styles.infoText}>
-        {`
-          Prank calls are traceable and will be investigated. They are a serious abuse of the Sask911 system.
-          Prank calls are traceable and will be investigated.
-          All incoming calls are recorded. The use of profanity or abuse of the telephone system may result in restrictions on phone service, or charges under the Criminal Code.
-          If emergency vehicles are dispatched for no reason, the prank caller may be liable for the costs involved.
-        `}
+{`\u2022 Prank calls are traceable and will be investigated. They are a serious abuse of the Sask911 system.
+\u2022 Prank calls are traceable and will be investigated.
+\u2022 All incoming calls are recorded. The use of profanity or abuse of the telephone system may result in restrictions on phone service, or charges under the Criminal Code.
+\u2022 If emergency vehicles are dispatched for no reason, the prank caller may be liable for the costs involved.
+`}
       </Text>
       <Text style={styles.subheaderText}>
-        Additional Information
+        Additional Information❓
       </Text>
-      <Text style={{color: 'blue', textDecorationLine: "underline"}}
+      <Text style={{color: 'blue', textDecorationLine: "underline", fontSize: 24,}}
           onPress={() => Linking.openURL('https://www.saskatchewan.ca/residents/emergency/sask911#911-safety-tips')}>
           Sask 911
+      </Text>
+      <Text>
+        {`\n\n`}
       </Text>
     </ScrollView>
   );
@@ -102,7 +99,8 @@ const ScriptScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50
+    marginTop: 50,
+    padding: 15,
   },
   headerText: {
     fontWeight: "bold",
@@ -117,6 +115,9 @@ const styles = StyleSheet.create({
   infoText:{
     fontSize: 24,
     textAlign: "justify",
+  },
+  emojis:{
+    textAlign: "center",
   }
 });
 export default ScriptScreen;
